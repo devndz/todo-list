02719db650f035518b13c9bafc8d5b9a55206ade
ndz <ndz@sfu.ca>
2026-07-22 17:54:34 -0700

    Added user login endpoint with JWT

--- src/routes/auth_router.ts
+++ src/routes/auth_router.ts
@@ -1,7 +1,9 @@
 import { type Request, type Response, Router } from "express";
 import prisma from "../prisma_setup/prisma.js";
 import { Prisma } from "../generated/prisma/client.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const router: Router = Router();
 
@@ -32,14 +34,45 @@ router.post("/registerUser", async (req: Request, res: Response) => {
         }
 
         if (error instanceof Prisma.PrismaClientValidationError) {
-            return res.status(400).json({ error: 'email, username or password were not provided.' });
+            return res.status(400).json({ error: 'email, username or password were not provided.'});
         }
 
         console.error(error);   
         res.status(500).json({ error: 'Internal server error' });
     }
-
-    
 } )
+
+//Cannot wait until the new query method from http gets added to express, for now gotta use a post non semantically
+router.post("/login", async (req: Request, res: Response) => {
+    try{
+        const {username, password} = req.body;
+
+        const exisitngUser = await prisma.user.findUnique({
+            where: {username}
+        });
+
+        if(!exisitngUser){
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        const correctPassword = await bcrypt.compare(password, exisitngUser.password);
+
+        if(!correctPassword){
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+        
+        const authToken = jwt.sign(
+            {id: exisitngUser.id, username: exisitngUser.username},
+            process.env.JWT_SECRET as string,
+            {expiresIn: "2h"}
+        )
+
+        return res.status(200).json({authToken});
+
+    } catch (error: any){
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
 
 export default router;
